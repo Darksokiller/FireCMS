8bb22dd414c03a60aeb395492ccaea76d8b0f4b6
Darksokiller <eevansocom@gmail.com>
2021-12-05 20:22:02 -0600

    starting to adding register and Login

---
+++ module/User/src/Controller/UserController.php
@@ -0,0 +1,39 @@
+<?php
+namespace User\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+
+class UserController extends AbstractActionController
+{
+    
+    public function indexAction()
+    {
+        
+    }
+    
+    public function registerAction()
+    {
+        
+    }
+    
+    public function editAction()
+    {
+        
+    }
+    
+    public function deleteAction()
+    {
+        
+    }
+    
+    public function logoutAction()
+    {
+        
+    }
+    
+    public function loginAction()
+    {
+        
+    }
+    
+}
