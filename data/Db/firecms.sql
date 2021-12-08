a3d0600a3fce95e00651979feb1cacfb9cf323b3
Darksokiller <eevansocom@gmail.com>
2021-12-08 17:12:41 -0600

    updated the firecms.sql file to the updated database

--- data/Db/firecms.sql
+++ data/Db/firecms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 06, 2021 at 06:25 PM
+-- Generation Time: Dec 08, 2021 at 11:10 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.4.9
 
@@ -60,14 +60,54 @@ CREATE TABLE IF NOT EXISTS `user` (
   `active` int(11) DEFAULT NULL,
   `verified` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `userName`, `email`, `password`, `role`, `regDate`, `active`, `verified`) VALUES
-(1, 'Evan Alexander', 'eevansocom@gmail.com', '$2y$10$9DfVLmJaN72AahI1OwPalOTW2FocnRkRWHsbuGtooRCbNDMwE3nVO', NULL, '2021-12-06 11:44:51', 1, 1);
+(1, 'Fire1659', 'eevansocom@gmail.com', '$2y$10$9DfVLmJaN72AahI1OwPalOTW2FocnRkRWHsbuGtooRCbNDMwE3nVO', 'guest', '2021-12-06 11:44:51', 1, 1),
+(2, 'test2', 'test2@domain.com', '$2y$10$o6NLaRcRVCLC9bVqadv0peVPLJU.DluXHLDdHW8Fdthzchh5T6JD6', NULL, NULL, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profile`
+--
+
+DROP TABLE IF EXISTS `user_profile`;
+CREATE TABLE IF NOT EXISTS `user_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `avatarPath` mediumtext,
+  `age` int(11) DEFAULT NULL,
+  `birthday` varchar(10) DEFAULT NULL,
+  `gender` varchar(6) DEFAULT NULL,
+  `race` varchar(255) DEFAULT NULL,
+  `bio` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userid` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+INSERT INTO `user_profile` (`id`, `userid`, `firstName`, `lastName`, `avatarPath`, `age`, `birthday`, `gender`, `race`, `bio`) VALUES
+(1, 1, 'Evan', 'Alexander', 'test', 22, '05/25/1999', 'male', 'white', 'test test');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD CONSTRAINT `user_profile_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
